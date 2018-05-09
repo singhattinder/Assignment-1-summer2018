ee4488bd0fb1bd9f91d01441ecd5eee4a5926f0b
Attinderpal <singh.at@husky.neu.edu>
2018-05-09 18:43:27 -0400

    first commit

---
+++ src/main/java/com/example/myapp/repositories/HelloRepository.java
@@ -0,0 +1,9 @@
+package com.example.myapp.repositories;
+
+import com.example.myapp.models.Hello;
+import org.springframework.data.repository.CrudRepository;
+
+public interface HelloRepository extends CrudRepository<Hello, Integer> {
+
+    
+}
