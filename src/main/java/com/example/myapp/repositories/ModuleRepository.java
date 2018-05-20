1d4c958a70a1769d813f40178664c7a511ac489f
Attinderpal <singh.at@husky.neu.edu>
2018-05-20 16:29:38 -0400

    Fixed module repo and added ModuleServices

---
+++ src/main/java/com/example/myapp/repositories/ModuleRepository.java
@@ -0,0 +1,21 @@
+package com.example.myapp.repositories;
+
+import com.example.myapp.models.Course;
+import com.example.myapp.models.Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+public interface ModuleRepository extends CrudRepository<Module, Integer> {
+
+
+
+
+
+
+
+
+
+}
