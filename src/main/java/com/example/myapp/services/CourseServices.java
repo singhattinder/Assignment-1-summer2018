fdc120d6a336509f2b2d54c558690449485fe08b
Attinderpal <singh.at@husky.neu.edu>
2018-05-20 00:10:33 -0400

    not working on heroku

--- src/main/java/com/example/myapp/services/CourseServices.java
+++ src/main/java/com/example/myapp/services/CourseServices.java
@@ -4,6 +4,7 @@ package com.example.myapp.services;
 import com.example.myapp.models.Course;
 import com.example.myapp.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,8 +13,12 @@ public class CourseServices {
     @Autowired
     CourseRepository courseRepository;
 
+    @GetMapping("/api/course")
     public Iterable<Course> findAllCourses(){
 
         return courseRepository.findAll();
     }
+
+
+
 }
