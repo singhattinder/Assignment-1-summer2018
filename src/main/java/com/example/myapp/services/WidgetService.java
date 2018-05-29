9222a6c69a409109b69a04c229b327b5d51abd03
Attinderpal <singh.at@husky.neu.edu>
2018-05-29 19:40:08 -0400

    Post mapping for saveAllWidgets

--- src/main/java/com/example/myapp/services/WidgetService.java
+++ src/main/java/com/example/myapp/services/WidgetService.java
@@ -4,20 +4,35 @@ package com.example.myapp.services;
 import com.example.myapp.models.Widget;
 import com.example.myapp.repositories.WidgetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class WidgetService {
 
 
     @Autowired
     WidgetRepository widgetRepository;
 
+    @GetMapping("/api/widget")
     public List<Widget> findAllWidgets(){
 
         return (List<Widget>) widgetRepository.findAll();
 
     }
+
+    @PostMapping("/api/widget/save")
+    public void saveAllWidgets(@RequestBody List<Widget> widgets){
+
+        for (Widget widget: widgets
+             ) {
+            widgetRepository.save(widget);
+            
+        }
+
+    }
+
+
 }
